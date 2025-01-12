3014bd752a884cb7ca1acb2b0bea12fe8a1b17ce
hmccarthy1 <huntermccarthy56@hotmail.com>
2025-01-12 09:56:12 -0600

    add basic button

---
+++ Button/Button/Button.tsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+
+export interface ButtonProps {
+  ButtonText: string;
+  useDarkMode: boolean;
+  size: string;
+  typeVariant: string;
+}
+
+export default function ButtonComponent(props: ButtonProps) {
+  
+  console.log("PROPS PASSED: ", props)
+
+  const theme = createTheme({
+    palette: {
+      mode: props.useDarkMode ? "dark" : "light" ,
+    },
+  });
+
+  const buttonSize = props.size == "small" ? "small" : props.size == "medium" ? "medium" : props.size == "large" ? "large" : "small"
+  const buttonVariant = props.typeVariant == "contained" ? "contained" : props.typeVariant == "outlined" ? "outlined" : "contained"
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+
+      <Button size = {  buttonSize }  variant = {buttonVariant}> {props.ButtonText} </Button>
+    </ThemeProvider>
+  );
+}
---
+++ Button/Button/index.ts
@@ -0,0 +1,51 @@
+import { IInputs, IOutputs } from "./generated/ManifestTypes";
+import ButtonComponent, { ButtonProps } from "./Button";
+import * as React from "react";
+
+export class Button implements ComponentFramework.ReactControl<IInputs, IOutputs> {
+    private theComponent: ComponentFramework.ReactControl<IInputs, IOutputs>;
+    private notifyOutputChanged: () => void;
+
+    
+    constructor() { }
+
+ 
+    public init(
+        context: ComponentFramework.Context<IInputs>,
+        notifyOutputChanged: () => void,
+        state: ComponentFramework.Dictionary
+    ): void {
+        this.notifyOutputChanged = notifyOutputChanged;
+    }
+
+    
+    public updateView(context: ComponentFramework.Context<IInputs>): React.ReactElement {
+
+        const props : ButtonProps = {
+            ButtonText: context.parameters.buttonText.raw || "Click me",
+            useDarkMode: context.parameters.DarkMode.raw,
+            size: context.parameters.sizeVariant.raw || "small",
+            typeVariant: context.parameters.typeVariant.raw || "contained"
+        }
+
+        return React.createElement(
+           ButtonComponent, props
+        );
+    }
+
+    /**
+     * It is called by the framework prior to a control receiving new data.
+     * @returns an object based on nomenclature defined in manifest, expecting object[s] for property marked as "bound" or "output"
+     */
+    public getOutputs(): IOutputs {
+        return { };
+    }
+
+    /**
+     * Called when the control is to be removed from the DOM tree. Controls should use this call for cleanup.
+     * i.e. cancelling any pending remote calls, removing listeners, etc.
+     */
+    public destroy(): void {
+        // Add code to cleanup control if necessary
+    }
+}
