95a967c8b8396e53c6c4a9c6e0624a76fba1422b
= <=>
2025-12-13 02:46:03 -0600

    area chart

--- ShadCnAreaChart/ShadCnAreaChart/HelloWorld.tsx
+++ ShadCnAreaChart/ShadCnAreaChart/HelloWorld.tsx
@@ -1,18 +1,29 @@
 "use client"
-
-import { Bar, BarChart } from "recharts"
+/* eslint-disable */
+import * as React from "react"
+import { TrendingUp } from "lucide-react"
+import { Area, AreaChart, CartesianGrid, XAxis } from "recharts"
 
 import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../components/ui/card"
+import {
   ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
   type ChartConfig,
 } from "../components/ui/chart"
-import React from "react"
 
+export const description = "An area chart with gradient fill"
 export interface AreaChartProps {
-  height?: number | string
-  width?: number | string
+  width: number
+  height: number
 }
-
 const chartData = [
   { month: "January", desktop: 186, mobile: 80 },
   { month: "February", desktop: 305, mobile: 200 },
@@ -25,28 +36,102 @@ const chartData = [
 const chartConfig = {
   desktop: {
     label: "Desktop",
-    color: "#2563eb",
+    color: "var(--chart-1)",
   },
   mobile: {
     label: "Mobile",
-    color: "#60a5fa",
+    color: "var(--chart-2)",
   },
 } satisfies ChartConfig
 
-export default function AreaChart(props: AreaChartProps) {
+export default function AreaChartComponent() {
 
-  
-  console.log('AreaChart props', props);
+  document.documentElement.classList.add("dark")
 
   return (
-    <div style={{height: `${props.height}px`, width: `${props.width}px`, minHeight: '500px', minWidth: '300px'}}>
-
-    <ChartContainer config={chartConfig} className="min-h-[200px] w-full" style={{height: '100%', width: '100%'}}>
-      <BarChart accessibilityLayer data={chartData}>
-        <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
-        <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
-      </BarChart>
-    </ChartContainer>
-    </div>
+    <Card className="bg-gray-950">
+      <CardHeader className="dark:text-white">
+        <CardTitle className="dark:text-white">Area Chart - Gradient</CardTitle>
+        <CardDescription className="dark:text-white">
+          Showing total visitors for the last 6 months
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <ChartContainer config={chartConfig}>
+          <AreaChart
+            accessibilityLayer
+            data={chartData}
+            margin={{
+              left: 12,
+              right: 12,
+            }}
+          >
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey="month"
+              tickLine={false}
+              axisLine={false}
+              tickMargin={8}
+              tickFormatter={(value) => value.slice(0, 3)}
+            />
+            <ChartTooltip labelClassName="dark:text-white" cursor={false} content={<ChartTooltipContent />}  />
+            <defs>
+              <linearGradient id="fillDesktop" x1="0" y1="0" x2="0" y2="1">
+                <stop
+                  offset="5%"
+                  stopColor="var(--color-desktop)"
+                  stopOpacity={0.8}
+                />
+                <stop
+                  offset="95%"
+                  stopColor="var(--color-desktop)"
+                  stopOpacity={0.1}
+                />
+              </linearGradient>
+              <linearGradient id="fillMobile" x1="0" y1="0" x2="0" y2="1">
+                <stop
+                  offset="5%"
+                  stopColor="var(--color-mobile)"
+                  stopOpacity={0.8}
+                />
+                <stop
+                  offset="95%"
+                  stopColor="var(--color-mobile)"
+                  stopOpacity={0.1}
+                />
+              </linearGradient>
+            </defs>
+            <Area
+              dataKey="mobile"
+              type="natural"
+              fill="url(#fillMobile)"
+              fillOpacity={0.4}
+              stroke="var(--color-mobile)"
+              stackId="a"
+            />
+            <Area
+              dataKey="desktop"
+              type="natural"
+              fill="url(#fillDesktop)"
+              fillOpacity={0.4}
+              stroke="var(--color-desktop)"
+              stackId="a"
+            />
+          </AreaChart>
+        </ChartContainer>
+      </CardContent>
+      <CardFooter>
+        <div className="flex w-full items-start gap-2 text-sm dark:text-white">
+          <div className="grid gap-2">
+            <div className="flex items-center gap-2 leading-none font-medium dark:text-white">
+              Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
+            </div>
+            <div className="text-muted-foreground flex items-center gap-2 leading-none dark:text-white">
+              January - June 2024
+            </div>
+          </div>
+        </div>
+      </CardFooter>
+    </Card>
   )
 }
